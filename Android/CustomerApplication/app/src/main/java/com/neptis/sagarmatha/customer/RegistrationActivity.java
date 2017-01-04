60f02f5f14f736535eca7316f56363aa1bff19c3
Mannik01 <mannik456@gmail.com>
2017-01-04 15:22:07 -0600

    added RegistrationActivity

---
+++ Android/CustomerApplication/app/src/main/java/com/neptis/sagarmatha/customer/RegistrationActivity.java
@@ -0,0 +1,17 @@
+package com.neptis.sagarmatha.customer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+/**
+ * Class for user registration
+ */
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
